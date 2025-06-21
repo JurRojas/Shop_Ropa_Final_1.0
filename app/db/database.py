d88ee3090c73543d8d8b562f39168861542b822b
Jesus Rojas <jesus.a.rojasr5@gmail.com>
2025-06-21 11:18:43 -0600

    Configuración para la bd

--- app/db/database.py
+++ app/db/database.py
@@ -11,7 +11,7 @@ MYSQL_USER = "root"
 MYSQL_PASSWORD = ""
 MYSQL_HOST = "localhost"
 MYSQL_PORT = "3306"
-MYSQL_DB = "shop_escuela"
+MYSQL_DB = "shop"
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
